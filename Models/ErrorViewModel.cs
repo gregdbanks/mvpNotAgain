9dfc8ce5067b5b1c2e695a5ae52fa1eadebea39d
Greg Banks <gregbanks@Gregs-MacBook-Pro.local>
2021-10-14 02:42:38 -0500

    f

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mostVividMonkey.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
